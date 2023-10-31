c7e12365158dc0a85adbb5bdeac32c78f32bb3c7
Ashish Singh Chilwal <76940534+ashuchilwal@users.noreply.github.com>
2023-10-31 19:57:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -224,7 +224,7 @@ def download_processed_df(df_to_download):
 
 
 def main():
-    st.title("Feature Engineering App")
+    st.title("Feature Engineering Application")
 
     # Upload dataset
     st.header("Upload Dataset")
